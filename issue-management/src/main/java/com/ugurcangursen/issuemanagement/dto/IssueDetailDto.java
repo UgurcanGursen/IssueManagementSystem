5843d83671c0d160dc14b42855ccaf0607fc6e5b
UgurcanGursen <ugurcangursen@gmail.com>
2019-11-13 12:21:49 +0300

    Issue History Service

---
+++ issue-management/src/main/java/com/ugurcangursen/issuemanagement/dto/IssueDetailDto.java
@@ -0,0 +1,108 @@
+package com.ugurcangursen.issuemanagement.dto;
+
+import com.ugurcangursen.issuemanagement.entity.IssueStatus;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.util.Date;
+import java.util.List;
+
+@ApiModel(value = "Issue Data Transfer Object")
+public class IssueDetailDto {
+
+    @ApiModelProperty(required = true,value = "ID")
+    private Long id;
+    @ApiModelProperty(required = true,value = "Description")
+    private String description;
+    @ApiModelProperty(required = true,value = "Issue Details")
+    private String details;
+    @ApiModelProperty(required = true,value = "Date")
+    private Date date;
+    @ApiModelProperty(required = true,value = "Issue Status")
+    private IssueStatus issueStatus;
+    @ApiModelProperty(required = true,value = "Assignee")
+    private UserDto assignee;
+    @ApiModelProperty(required = true,value = "Project")
+    private ProjectDto project;
+
+    @ApiModelProperty(required = true,value = "Issue Histories")
+    private List<IssueHistoryDto> issueHistories;
+
+    public IssueDetailDto() {
+    }
+
+    public IssueDetailDto(Long id, String description, String details, Date date, IssueStatus issueStatus, UserDto assignee, ProjectDto project, List<IssueHistoryDto> issueHistories) {
+        this.id = id;
+        this.description = description;
+        this.details = details;
+        this.date = date;
+        this.issueStatus = issueStatus;
+        this.assignee = assignee;
+        this.project = project;
+        this.issueHistories = issueHistories;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public IssueStatus getIssueStatus() {
+        return issueStatus;
+    }
+
+    public void setIssueStatus(IssueStatus issueStatus) {
+        this.issueStatus = issueStatus;
+    }
+
+    public UserDto getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(UserDto assignee) {
+        this.assignee = assignee;
+    }
+
+    public ProjectDto getProject() {
+        return project;
+    }
+
+    public void setProject(ProjectDto project) {
+        this.project = project;
+    }
+
+    public List<IssueHistoryDto> getIssueHistories() {
+        return issueHistories;
+    }
+
+    public void setIssueHistories(List<IssueHistoryDto> issueHistories) {
+        this.issueHistories = issueHistories;
+    }
+}
