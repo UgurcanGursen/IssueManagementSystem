c45bcbbd3abd63d97010b90fb3c004a46f76f51d
UgurcanGursen <ugurcangursen@gmail.com>
2019-11-13 16:15:47 +0300

    AccountController and Authentication API

--- issue-management/src/main/java/com/ugurcangursen/issuemanagement/service/impl/UserServiceImpl.java
+++ issue-management/src/main/java/com/ugurcangursen/issuemanagement/service/impl/UserServiceImpl.java
@@ -3,9 +3,14 @@ package com.ugurcangursen.issuemanagement.service.impl;
 import java.util.Arrays;
 import java.util.List;
 
+import com.ugurcangursen.issuemanagement.advice.IMExceptionHandler;
+import com.ugurcangursen.issuemanagement.dto.RegistrationRequest;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.ugurcangursen.issuemanagement.dto.UserDto;
@@ -13,18 +18,21 @@ import com.ugurcangursen.issuemanagement.entity.User;
 import com.ugurcangursen.issuemanagement.repository.UserRepository;
 import com.ugurcangursen.issuemanagement.service.UserService;
 import com.ugurcangursen.issuemanagement.util.TPage;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private static final Logger LOGGER = LoggerFactory.getLogger(IMExceptionHandler.class); // Logging APIs
 
-    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
         this.modelMapper = modelMapper;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
-
     @Override
     public UserDto save(UserDto user) {
         User u = modelMapper.map(user, User.class);
@@ -57,5 +65,21 @@ public class UserServiceImpl implements UserService {
     public UserDto getByUsername(String username) {
         User u = userRepository.findByUsername(username);
         return modelMapper.map(u, UserDto.class);
+    }
+
+    @Transactional
+    public Boolean register(RegistrationRequest registrationRequest) {
+        try {
+            User user = new User();
+            user.setEmail(registrationRequest.getEmail());
+            user.setNameSurname(registrationRequest.getNameSurname());
+            user.setPassword(bCryptPasswordEncoder.encode(registrationRequest.getPassword()));
+            user.setUsername(registrationRequest.getUsername());
+            userRepository.save(user);
+            return Boolean.TRUE;
+        } catch (Exception e) {
+            LOGGER.error("REGISTRATION=>", e);
+            return Boolean.FALSE;
+        }
     }
 }
